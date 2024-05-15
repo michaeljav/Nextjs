37789c1693b97d933db7c8078365cb4389ca4b6e
Michael Javier <m.mota@concentra.com.do>
2024-05-15 08:33:49 -0400

    creado otro projecto  para probar next code

---
+++ CRUD_MARKETPLACE/crudnext/src/app/admin/product/add/page.tsx
@@ -0,0 +1,6 @@
+import ProductAddForm from "@/components/ProductAddForm";
+import React from "react";
+
+export default function ProductAddPage() {
+  return <ProductAddForm />;
+}
--- next_testing_code/src/app/layout.tsx
+++ next_testing_code/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import AppContainer from "@/components/AppContainer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* hue saturation light and alpha hsla()*/}
-      <body
-        className={inter.className}
-        style={{ backgroundColor: "hsla(0,0%,95%,1.0)" }}
-      >
-        <AppContainer>{children}</AppContainer>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
